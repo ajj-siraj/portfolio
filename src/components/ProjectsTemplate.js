69ed4735e9a1118298b9ffb0bba6e5211501457c
Sirag Ahmed <ajj.siraj@gmail.com>
2020-08-02 11:27:59 +0200

    projects styling

--- src/components/ProjectsTemplate.js
+++ src/components/ProjectsTemplate.js
@@ -11,15 +11,69 @@ import * as Animations from "./animations";
 //images & vectors
 import github from "../images/logos/github_logo.svg";
 import globe from "../images/logos/globe.svg";
+//technologies
+import html5 from "../images/logos/HTML5_logo.svg";
+import css3 from "../images/logos/css3_logo.svg";
+import bootstrap4 from "../images/logos/bootstrap4_logo.svg";
+import express from "../images/logos/express_logo.svg";
+import git from "../images/logos/git_logo.svg";
+import javascript from "../images/logos/javascript_logo.svg";
+import jquery from "../images/logos/jquery_logo.svg";
+import angular from "../images/logos/angular_logo.svg";
+import mongodb from "../images/logos/mongodb_logo.svg";
+import mysql from "../images/logos/mysql_logo.svg";
+import nextjs from "../images/logos/nextjs_logo.svg";
+import nodejs from "../images/logos/nodejs_logo.svg";
+import php from "../images/logos/php_logo.svg";
+import python from "../images/logos/python_logo.svg";
+import react from "../images/logos/react_logo.svg";
+import reactNative from "../images/logos/react_logo.svg";
+import redux from "../images/logos/redux_logo.svg";
+import typescript from "../images/logos/typescript_logo.svg";
+import xamarin from "../images/logos/xamarin_logo.svg";
+import gatsbyjs from "../images/logos/gatsby.svg";
 
 //css modules
 import projectStyles from "../css/projects.css";
+
+const imgs = {
+  html5: html5,
+  css3: css3,
+  bootstrap4: bootstrap4,
+  javascript: javascript,
+  react: react,
+  reactNative: reactNative,
+  redux: redux,
+  git: git,
+  jquery: jquery,
+  nodejs: nodejs,
+  express: express,
+  mongodb: mongodb,
+  nextjs: nextjs,
+  mysql: mysql,
+  php: php,
+  python: python,
+  typescript: typescript,
+  xamarin: xamarin,
+  angular: angular,
+  gatsbyjs: gatsbyjs,
+}
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Technologies = (props) => {
   console.log(props);
-  return <div></div>
+  let imgKeys = Object.keys(imgs);
+  let foundTechs = imgKeys.filter((imgKey, idx) => {
+    return props.techs.includes(imgKey.toString());
+  })
+  console.log("Foundtechs: ", foundTechs);
+  let techs = foundTechs.map((tech, idx) => {
+      return (
+        <img className="techSvg" src={imgs[tech]} />
+      );
+  })
+  return <div className="techsContainer">{techs}</div>
 }
 const ProjectsTemplate = props => {
   const [data, setData] = useState(null);
