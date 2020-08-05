854e7ca6cd22da8551e2537456fe60fcc13dd4d2
Sirag Ahmed <ajj.siraj@gmail.com>
2020-08-05 18:21:11 +0200

    add slick carousel and content to skills section

--- src/components/SkillSection.js
+++ src/components/SkillSection.js
@@ -4,6 +4,10 @@ import { Container, Row, Col, Tab, Tabs, Nav } from "react-bootstrap";
 import { Tween, Reveal, Timeline, Controls, PlayState } from "react-gsap";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Slider from "react-slick";
+
+//utility
+import { capitalizeFirstLetter } from "../Utility";
 
 //images
 import html5 from "../images/logos/HTML5_logo.svg";
@@ -21,6 +25,7 @@ import nodejs from "../images/logos/nodejs_logo.svg";
 import php from "../images/logos/php_logo.svg";
 import python from "../images/logos/python_logo.svg";
 import react from "../images/logos/react_logo.svg";
+import reactNative from "../images/logos/react_logo.svg";
 import redux from "../images/logos/redux_logo.svg";
 import typescript from "../images/logos/typescript_logo.svg";
 import xamarin from "../images/logos/xamarin_logo.svg";
@@ -29,32 +34,38 @@ import gatsbyjs from "../images/logos/gatsby.svg";
 //animations
 import * as Animations from "./animations";
 
-//css modules
+//css
 import skillStyles from "../css/skills.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const imgs = [
-  html5,
-  css3,
-  bootstrap4,
-  javascript,
-  react,
-  redux,
-  git,
-  jquery,
-  nodejs,
-  express,
-  mongodb,
-  nextjs,
-  mysql,
-  php,
-  python,
-  typescript,
-  xamarin,
-  angular,
-  gatsbyjs,
-];
+const learnedTech = {
+  html5: html5,
+  css3: css3,
+  bootstrap4: bootstrap4,
+  javascript: javascript,
+  react: react,
+  redux: redux,
+  git: git,
+  jquery: jquery,
+  nodejs: nodejs,
+  express: express,
+  mongodb: mongodb,
+  gatsbyjs: gatsbyjs,
+  mysql: mysql,
+};
+
+const toLearnTech = {
+  reactNative: reactNative,
+  nextjs: nextjs,
+  php: php,
+  python: python,
+  typescript: typescript,
+  xamarin: xamarin,
+  angular: angular,
+};
 
 const SkillSection = props => {
   let heading = useRef(null);
@@ -81,6 +92,96 @@ const SkillSection = props => {
     gsap.to(underline, Animations.lineUnhover);
   };
 
+  const responsiveSettings = [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+  ];
+
+  const settings1 = {
+    swipe: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 1500,
+    easing: "none",
+    cssEase: "linear",
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: responsiveSettings,
+  };
+
+  const settings2 = {
+    
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 1500,
+    easing: "none",
+    cssEase: "linear",
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: responsiveSettings,
+  };
+
+  //map the techs svgs to add to the sliders
+  let learnedValues = Object.values(learnedTech);
+  let learnedKeys = Object.keys(learnedTech);
+  let learned = learnedValues.map((svg, idx) => (
+    <div key={`tech-svg-${idx}`}>
+      <img
+        className="techSvg m-auto text-center"
+        src={svg}
+        alt={`tech-svg-${idx}`}
+        style={learnedKeys[idx] === "express" || "mongodb" || "jquery" ? {width: "150px"} : null}
+      />
+      <h3 className="tech-heading">
+        {capitalizeFirstLetter(learnedKeys[idx])}
+      </h3>
+    </div>
+  ));
+
+  
+  let toLearnKeys = Object.keys(toLearnTech);
+  let toLearnValues = Object.values(toLearnTech);
+  console.log("KEYS: ", toLearnKeys);
+  console.log("VALUES: ", toLearnValues);
+
+  let toLearn = toLearnValues.map((svg, idx) => {
+    return (
+      <div key={`tech-svg2-${idx}`}>
+        <img
+          className="techSvg m-auto text-center"
+          src={svg}
+          alt={`tech-svg2-${idx}`}
+          
+        />
+        <h3 className="tech-heading">
+          {capitalizeFirstLetter(toLearnKeys[idx])}
+        </h3>
+      </div>
+    );
+  });
+
   return (
     <Container fluid className="text-center align-items-center mb-1">
       {/* This Row is reserved for the heading */}
@@ -105,6 +206,22 @@ const SkillSection = props => {
               <span className="heading-underline" ref={underline}></span>
             </h1>
           </div>
+        </Col>
+      </Row>
+      <Row className="mt-5 mb-5">
+        <Col xs={12} className=" mb-5">
+          <h2 className="tech-heading-large mb-3">Technologies I feel comfortable with...</h2>
+        </Col>
+        <Col xs={12}>
+          <Slider {...settings1}>{learned}</Slider>
+        </Col>
+      </Row>
+      <Row className="mt-5 mb-5">
+        <Col xs={12} className="mt-5 mb-5">
+          <h2 className="tech-heading-large">Basic literacy or currently learning...</h2>
+        </Col>
+        <Col xs={12}>
+          <Slider {...settings2}>{toLearn}</Slider>
         </Col>
       </Row>
     </Container>
