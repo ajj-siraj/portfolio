6c2ce9b2d7121358af657d5d4179690c408681e1
Sirag Ahmed <ajj.siraj@gmail.com>
2020-08-09 14:27:41 +0200

    Contact form added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import React, {useEffect} from "react";
 import {gsap} from "gsap";
 
 //redux
-import { connect } from "react-redux";
-import * as Actions from "../state/actions";
+// import { connect } from "react-redux";
+// import * as Actions from "../state/actions";
 
 //components
 import Main from "../components/Main";
@@ -13,25 +13,21 @@ import Main from "../components/Main";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 //for redux
-const mapStateToProps = state => {
-  return {
-    loading: state.loading,
-  };
-};
+// const mapStateToProps = state => {
+//   return {
+//     loading: state.loading,
+//   };
+// };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchingData: () => dispatch(Actions.fetchingData()),
-    fetchingDone: () => dispatch(Actions.fetchingDone()),
-  };
-};
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     fetchingData: () => dispatch(Actions.fetchingData()),
+//     fetchingDone: () => dispatch(Actions.fetchingDone()),
+//   };
+// };
 
 const IndexPage = props => {
 
-  useEffect(() => {
-    console.log(document.childNodes[1]);
-    if(document) gsap.to(document.childNodes[1], {opacity: 1});
-  })
   return (
     <div>
       <Main {...props} />
@@ -39,5 +35,5 @@ const IndexPage = props => {
   );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(IndexPage);
-// export default IndexPage
+// export default connect(mapStateToProps, mapDispatchToProps)(IndexPage);
+export default IndexPage
